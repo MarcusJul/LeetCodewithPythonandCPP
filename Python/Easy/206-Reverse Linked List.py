50d9c2068110c322772839bba540a351c410d25b
MarcusJul <Lancecheung960425@hotmail.com>
2020-03-02 21:13:21 +0800

    2020.03.20

--- Python/Easy/206-Reverse Linked List.py
+++ Python/Easy/206-Reverse Linked List.py
@@ -6,21 +6,24 @@
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
+        if head==None: 
+            return None 
         store = []
         cur_node = head
         while(cur_node!=None):
             store.append(cur_node)
             cur_node = cur_node.next
-        head_node, cur_node = store[-1],store[-1]
+            
         store = list(reversed(store))
+        head_node, cur_node = store[0],store[0]
         
         while(True):
-            store = store[:-1]
+            store = store[1:]
             if store==[]:
                 break
             else:
-                cur_node.next = store[-1]
-                cur_node = store[-1]
+                cur_node.next = store[0]
+                cur_node = store[0]
                 
         cur_node.next = None
         return head_node
