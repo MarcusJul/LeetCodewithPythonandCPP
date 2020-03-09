a3b7ca54612641c8cbeaa5615bf71e1098943303
MarcusJul <Lancecheung960425@hotmail.com>
2020-03-09 21:53:02 +0800

    2020.03.09

---
+++ Python/Easy/136-Single Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d = dict()
+        for num in nums:
+            if num in d:
+                d[num]+=1
+            else:
+                d[num]=1
+        for k,v in d.items():
+            if v==1:
+                return k
