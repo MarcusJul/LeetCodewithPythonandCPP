5d39d490f5865edb151e5503c84701570a82c277
MarcusJul <lancecheung960425@hotmail.com>
2020-03-28 18:03:29 +0800

    2020.03.28

---
+++ Python/Easy/232-Implementing Queue using Stack.py
@@ -0,0 +1,47 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.s = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.s.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        ret = self.s[0]
+        self.s = self.s[1:]
+        return ret
+        
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.s[0]
+        
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        if self.s==[]:
+            return True
+        else:
+            return False 
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
