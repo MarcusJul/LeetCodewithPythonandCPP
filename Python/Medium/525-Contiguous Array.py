94188558007391a61260cd6298b5b6c5a0abbf0e
MarcusJul <lancecheung960425@hotmail.com>
2020-04-15 17:53:03 +0800

    2020.04.15

---
+++ Python/Medium/525-Contiguous Array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        
+        s = 0
+        l = len(nums)
+        presum = [0]*l
+        d = {0:-1}
+        m = 0
+        for i in range(l):
+            s += 1 if nums[i]==1 else -1
+            presum[i] = s
+            if s not in d:
+                d[s] = i
+            else:
+                if i-d[s]>m:
+                    m = i-d[s]
+        return m
+        
+        
